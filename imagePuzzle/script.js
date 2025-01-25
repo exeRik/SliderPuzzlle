429d35c084ff175dd285648ff0d994cd672cb94c
exeRik <smarika.p17@gmail.com>
2025-01-25 12:05:24 +0545

    puzzle basics movements developed

--- imagePuzzle/script.js
+++ imagePuzzle/script.js
@@ -1,6 +1,17 @@
 const imgElements = document.getElementsByTagName("img");
-const blankElement = document.getElementsByClassName("blank-element")[0];
+let  blankElement = document.getElementsByClassName("blank-element")[0];
 
+// clickedElement.setAttribute("data-row", blankRow);
+// clickedElement.setAttribute("data-col", blankCol);
+// blankElement.setAttribute("data-row", dataRow);
+// blankElement.setAttribute("data-col", dataCol);
+// clickedElement.setAttribute("src", blankSrc);
+// blankElement.setAttribute("src", dataSrc);
+
+let  blankRow = blankElement.getAttribute("data-row");
+let  blankCol = blankElement.getAttribute("data-col");
+let  blankSrc = blankElement.getAttribute("src");
+let  blankClass = blankElement.getAttribute("class");
 
 
 
@@ -10,110 +21,135 @@ for (let i = 0; i < imgElements.length; i++) {
     imgElements[i].addEventListener("click", function(event) {
       // Access the clicked image element
       let clickedImg = event.target;
-    //   console.log(clickedImg)
-    let  blankRow = blankElement.getAttribute("data-row");
-    let  blankCol = blankElement.getAttribute("data-col");
-    let  blankSrc = blankElement.getAttribute("src");
-    let  blankClass = blankElement.getAttribute("class");
+
+
+
+
+
+      // console.log(clickedImg)
+    // let  blankRow = parseInt(blankElement.getAttribute("data-row"));
+    // let  blankCol = parseInt(blankElement.getAttribute("data-col"));
+    // let  blankSrc = blankElement.getAttribute("src");
+    // let  blankClass = blankElement.getAttribute("class");
+    // console.log(blankElement)
       // Access the data attributes of the clicked image
       let  dataRow = clickedImg.getAttribute("data-row");
       let  dataCol = clickedImg.getAttribute("data-col");
       let  dataSrc = clickedImg.getAttribute("src");
       let  dataClass = clickedImg.getAttribute("class");
 
+   // now we swap the values of clicked element and blank element 
+
+   function swapElements(){
+    clickedImg.setAttribute("src" ,blankSrc)
+          blankElement.setAttribute("src" , dataSrc)
   
-      // Log or use the data attributes
-
-      //initaillay setting location of blankthinggy
-      
-    
-    // now we check for the blank thingy wrt to clicked elements index
-     if(dataRow == 3 && dataCol == 2){
-        if(blankRow == 3 && blankCol == 3) {
-
-            clickedImg.setAttribute("src" ,blankSrc)
-            blankElement.setAttribute("src" , dataSrc)
-            blankElement.setAttribute("data-row" , dataRow)
-            blankElement.setAttribute("data-col" , dataCol)
-            blankElement.setAttribute("class" , dataClass)
-            clickedImg.setAttribute("data-row" , blankRow)
-            clickedImg.setAttribute("data-col" , blankCol)
-            clickedImg.setAttribute("class" , blankClass)
-            // Swap blankRow and dataRow
-            console.log("Before Swap:", blankRow, dataRow);
-            [blankRow, dataRow] = [dataRow, blankRow];
-            [blankCol, dataCol] = [dataCol, blankCol];
-            console.log("After Swap:", blankRow, dataRow);
-            console.log("After Swap:", blankCol, dataCol);
-            // Log for debugging
-            console.log("Data:", dataRow, dataCol, blankRow, blankCol);
-            console.log(clickedImg)
- 
-
-        }
-
-     }
-     
-     if(dataRow == 2 && dataCol == 3){
-        if(blankRow == 3 && blankCol == 3) {
-            clickedImg.setAttribute("src" ,"images/blank.avif")
-            blankElement.setAttribute("src" , dataSrc)
-            blankElement.setAttribute("data-row" , dataRow)
-            blankElement.setAttribute("data-col" , dataCol)
-            blankElement.setAttribute("class" , dataClass)
-            clickedImg.setAttribute("data-row" , blankRow)
-            clickedImg.setAttribute("data-col" , blankCol)
-            clickedImg.setAttribute("class" , blankClass)
-            // Swap blankRow and dataRow
-            [blankRow, dataRow] = [dataRow, blankRow];
-            [blankCol, dataCol] = [dataCol, blankCol];
-            console.log(clickedImg)
+            let temp;     
+           temp = dataRow;
+           dataRow= blankRow;
+           blankRow = temp;
+  
+           temp = dataCol;
+           dataCol= blankCol;
+           blankCol = temp;
+         
+            blankElement = clickedImg;
+  }
 
 
-        }
+    // row 3
 
-     }
-     if(dataRow == 2 && dataCol == 2){
-        if(blankRow == 3 && blankCol == 2) {
-            clickedImg.setAttribute("src" ,blankSrc)
-            blankElement.setAttribute("src" , dataSrc)
-            blankElement.setAttribute("data-row" , dataRow)
-            blankElement.setAttribute("data-col" , dataCol)
-            blankElement.setAttribute("class" , dataClass)
-            clickedImg.setAttribute("data-row" , blankRow)
-            clickedImg.setAttribute("data-col" , blankCol)
-            clickedImg.setAttribute("class" , blankClass)
-            // Swap blankRow and dataRow
+   if(dataRow == 3 && dataCol == 2  && blankRow == 3 && blankCol == 3 ) {
+    swapElements(clickedImg, blankElement);
+   }
+   else if(dataRow == 3 && dataCol == 3  && blankRow == 3 && blankCol == 2 ) {
+    swapElements(clickedImg, blankElement);
+   }
+   else if(dataRow == 3 && dataCol == 1  && blankRow == 3 && blankCol == 2 ) {
+    swapElements(clickedImg, blankElement);
+   }
+   else if(dataRow == 3 && dataCol == 2  && blankRow == 3 && blankCol == 1 ) {
+    swapElements(clickedImg, blankElement);
+   }
+    // row 2 swap
+  else if(dataRow == 2 && dataCol == 2  && blankRow == 3 && blankCol == 2 ) {
+      swapElements(clickedImg, blankElement);
+   }
+  else if(dataRow == 3 && dataCol == 2  && blankRow == 2 && blankCol == 2 ) {
+      swapElements(clickedImg, blankElement);
+   }
+  else if(dataRow == 2 && dataCol == 1  && blankRow == 2 && blankCol == 2 ) {
+    swapElements(clickedImg, blankElement);
+ }
+ else if(dataRow == 2 && dataCol == 2  && blankRow == 2 && blankCol == 1) {
+  swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 2 && dataCol == 3  && blankRow == 2 && blankCol == 2 ) {
+  swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 2 && dataCol == 2  && blankRow == 2 && blankCol == 3) {
+swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 2 && dataCol == 1  && blankRow == 3 && blankCol == 1 ) {
+  swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 3 && dataCol == 1  && blankRow == 2 && blankCol == 1) {
+swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 3 && dataCol == 3  && blankRow == 2 && blankCol == 3 ) {
+  swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 2 && dataCol == 3  && blankRow == 3 && blankCol == 3) {
+  swapElements(clickedImg, blankElement);
+}
 
-            [blankRow, dataRow] = [dataRow, blankRow];
-            console.log(clickedImg)
+// swap row -1
+
+else if(dataRow == 1 && dataCol == 2  && blankRow == 2 && blankCol == 2 ) {
+  swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 2 && dataCol == 2  && blankRow == 1 && blankCol == 2) {
+  swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 1 && dataCol == 1  && blankRow == 1 && blankCol == 2 ) {
+  swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 1 && dataCol == 2  && blankRow == 1 && blankCol == 1) {
+  swapElements(clickedImg, blankElement);
+}
+
+else if(dataRow == 1 && dataCol == 1  && blankRow == 2 && blankCol == 1 ) {
+  swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 2 && dataCol == 1  && blankRow == 1 && blankCol == 1) {
+  swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 1 && dataCol == 3  && blankRow == 1 && blankCol == 2 ) {
+  swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 1 && dataCol == 2  && blankRow == 1 && blankCol == 3) {
+  swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 1 && dataCol == 3  && blankRow == 2 && blankCol == 1 ) {
+  swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 2 && dataCol == 1  && blankRow == 1 && blankCol == 3) {
+  swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 2 && dataCol == 3  && blankRow == 1 && blankCol == 3 ) {
+  swapElements(clickedImg, blankElement);
+}
+else if(dataRow == 1 && dataCol == 3  && blankRow == 2 && blankCol == 3) {
+  swapElements(clickedImg, blankElement);
+}
 
 
 
-        }
-         else if(blankRow == 2 && blankCol == 3) {
-            clickedImg.setAttribute("src" ,blankSrc)
-            blankElement.setAttribute("src" , dataSrc)
-            blankElement.setAttribute("data-row" , dataRow)
-            blankElement.setAttribute("data-col" , dataCol)
-            blankElement.setAttribute("class" , dataClass)
-            clickedImg.setAttribute("data-row" , blankRow)
-            clickedImg.setAttribute("data-col" , blankCol)
-            clickedImg.setAttribute("class" , blankClass)
-            // Swap blankRow and dataRow
 
-            [blankRow, dataRow] = [dataRow, blankRow];
-            console.log(clickedImg)
-
-
-
-        }
-
-     }
 
    
-
-    });
-  }
+           
+});
+}
 
   
